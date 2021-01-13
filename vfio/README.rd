0b58eabe4914335685a9d9f3671fcc0ff71f5264
brad <bwhorton@localhost.localdomain>
2021-01-13 14:01:40 -0600

    update vfio with guide

---
+++ vfio/README.rd
@@ -0,0 +1,2 @@
+Guide that worked for me:
+https://github.com/ekistece/Fedora-33-VFIO-guide/
--- vfio/iommu.sh
+++ vfio/iommu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-shopt -s nullglob
-for g in `find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V`; do
-    echo "IOMMU Group ${g##*/}:"
-    for d in $g/devices/*; do
-        echo -e "\t$(lspci -nns ${d##*/})"
-    done;
-done;
+for d in /sys/kernel/iommu_groups/*/devices/*; do
+  n=${d#*/iommu_groups/*}; n=${n%%/*}
+  printf 'IOMMU Group %s ' "$n"
+  lspci -nns "${d##*/}"
+done
+
